6b44ddb6b203e51133d4f87066f47bd4b5d2fe07
Christian Jantz <cj.ase@icloud.com>
2024-01-22 08:20:28 +0100

    update imports

--- batchapi.py
+++ batchapi.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from ast import arg
-from calendar import c
+
 from configparser import ConfigParser
 import azure.batch.models as batchmodels
 import argparse
